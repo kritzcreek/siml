580e56989e4feaef0c3b13e4a3be49ae4b68a998
kritzcreek <christoph.hegemann1337@gmail.com>
2019-04-08 17:01:28 +0200

    adds some debugging/testing helpers

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,17 @@ extern crate siml;
 use siml::token;
 use siml::grammar;
 
-fn main() {
-    let input = "\\y. x y z";
-    let lexer = token::Lexer::new(&input);
-    /*
-    lexer.for_each(|t| {
-        println!("{:?}", t)
-    });
-    */
+fn parse_expr(input: &str) {
+    let lexer = token::Lexer::new(input);
+    let res = grammar::ExprParser::new().parse(lexer);
+    println!("{:?}", &res);
+    println!("{:?}", res.map(|e| e.print()));
+}
 
-    println!("{:?}", grammar::ExprParser::new().parse(lexer))
+fn main() {
+    parse_expr("x");
+    parse_expr("\\y. x y z");
+    parse_expr("\\y. x (\\z.z)");
+    parse_expr("x (y z)");
+    parse_expr("(x x) (y z)");
 }
