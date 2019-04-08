637ee731d404e28e3bccdebd65f38468168666a7
kritzcreek <christoph.hegemann1337@gmail.com>
2019-04-08 17:01:15 +0200

    adds an expression printer

--- src/ast.rs
+++ src/ast.rs
@@ -4,3 +4,29 @@ pub enum Expr {
     Lambda { binder: String, body: Box<Expr> },
     Var(String),
 }
+
+impl Expr {
+    pub fn print(&self) -> String {
+        self.print_inner(0)
+    }
+
+    fn print_inner(&self, depth: u32) -> String {
+        match self {
+            Expr::Var(s) => s.clone(),
+            Expr::Lambda {binder, body} =>
+                format!("(\\{}. {})", binder, body.print()),
+            Expr::App {func, arg} => {
+                parens_if(depth>0, format!("{} {}", func.print_inner(depth), arg.print_inner(depth+1)))
+            }
+        }
+    }
+
+}
+
+fn parens_if(p: bool, s: String) -> String {
+    if p {
+        format!("({})", s)
+    } else {
+        s
+    }
+}
