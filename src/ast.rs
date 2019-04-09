8172c921aada6e706a4e30ee3de09fe2169e11e6
kritzcreek <christoph.hegemann1337@gmail.com>
2019-04-10 01:30:56 +0200

    wip subst

--- src/ast.rs
+++ src/ast.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub enum Expr {
     App { func: Box<Expr>, arg: Box<Expr> },
@@ -21,6 +23,47 @@ impl Expr {
         }
     }
 
+    pub fn free_vars(&self) -> HashSet<&String> {
+        match self {
+            Expr::Var(s) => {
+                let mut res = HashSet::new();
+                res.insert(s);
+                res
+            },
+            Expr::Lambda{binder, body} => {
+                let mut res = body.free_vars();
+                res.remove(binder);
+                res
+            },
+            Expr::App{ func, arg } => {
+                func.free_vars().union(&arg.free_vars()).cloned().collect()
+            },
+        }
+    }
+
+    pub fn substitute(&self, old: &String, new: &String) -> Expr {
+        match self {
+            Expr::Var(x) => { if old == x {
+                Expr::Var(new.clone())
+            } else {
+                Expr::Var(x.clone())
+            }}
+            Expr::Lambda{ binder, body } => {
+                let mut free_vars: HashSet<&String> = body.free_vars();
+                free_vars.remove(binder);
+                let new_body;
+                if free_vars.contains(&new) {
+                    new_body = body.substitute(old, &format!("{}'", new.clone()))
+                } else {
+                    new_body = body.substitute(old, new)
+                }
+                Expr::Lambda { binder: binder.to_string(), body: Box::new(new_body.clone()) }
+            }
+            Expr::App { func, arg } => {
+                Expr::App { func: Box::new(func.substitute(old, new).clone()), arg: Box::new(arg.substitute(old, new).clone()) }
+            }
+        }
+    }
 }
 
 fn parens_if(p: bool, s: String) -> String {
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 extern crate siml;
 use siml::token;
 use siml::grammar;
+use siml::ast::Expr;
 
-fn parse_expr(input: &str) {
+fn parse_expr(input: &str) -> Expr {
     let lexer = token::Lexer::new(input);
     let res = grammar::ExprParser::new().parse(lexer);
     println!("{:?}", &res);
-    println!("{:?}", res.map(|e| e.print()));
+    // println!("{:?}", &res.map(|e| e.print()));
+    res.unwrap()
 }
 
 fn main() {
@@ -15,4 +17,6 @@ fn main() {
     parse_expr("\\y. x (\\z.z)");
     parse_expr("x (y z)");
     parse_expr("(x x) (y z)");
+    let expr = parse_expr("\\z. (x x) (y z)");
+    println!("{}", expr.substitute(&"x".to_string(), &"y".to_string()).print())
 }
