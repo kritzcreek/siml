301a0b2637f73574288b63ad7cf7d442da9d76ac
kRITZCREEK <christoph.hegemann1337@gmail.com>
2019-06-20 09:42:10 +0200

    adds some type checker tests

---
+++ tests/types_tests.rs
@@ -0,0 +1,74 @@
+use siml::expr::{Expr, Literal};
+use siml::grammar::{ExprParser, TypeParser};
+use siml::token::Lexer;
+use siml::types::{Scheme, Substitution, Type, TypeChecker};
+
+fn ty(input: &str) -> Type {
+    let lexer = Lexer::new(input);
+    let res = TypeParser::new().parse(lexer);
+    match res {
+        Err(err) => panic!("{:?}", err),
+        Ok(ty) => ty,
+    }
+}
+
+fn expr(input: &str) -> Expr {
+    let lexer = Lexer::new(input);
+    let res = ExprParser::new().parse(lexer);
+    match res {
+        Err(err) => panic!("{:?}", err),
+        Ok(ty) => ty,
+    }
+}
+
+fn test_ty_eq_subst(ty1: Type, ty2: Type, subst: Substitution) {
+    assert_eq!(ty1.generalize(&subst), ty2.generalize(&subst))
+}
+
+fn test_ty_eq(ty1: Type, ty2: Type) {
+    test_ty_eq_subst(ty1, ty2, Substitution::new())
+}
+
+fn test_infer(expr: Expr, ty: Type) {
+    test_ty_eq(TypeChecker::new().infer_expr(&expr).unwrap(), ty)
+}
+
+#[test]
+fn it_infers_int_literals() {
+    test_infer(expr("1"), ty("Int"));
+    test_infer(expr("true"), ty("Bool"));
+    test_infer(expr("\\x. x"), ty("a -> a"));
+}
+
+#[test]
+fn it_generalizes() {
+    assert_eq!(
+        ty("a -> b").generalize(&Substitution::new()),
+        Scheme {
+            vars: vec!["gen0".to_string(), "gen1".to_string()],
+            ty: ty("gen0 -> gen1")
+        }
+    );
+}
+
+#[test]
+fn it_generalizes_while_ignoring_vars_in_the_subst() {
+    assert_eq!(
+        ty("a -> b").generalize(&Substitution::singleton("a".to_string(), ty("Int"))),
+        Scheme {
+            vars: vec!["gen0".to_string()],
+            ty: ty("a -> gen0")
+        }
+    );
+}
+
+#[test]
+fn it_figures_out_test_equality() {
+    test_ty_eq(ty("a -> b"), ty("b -> c"));
+
+    test_ty_eq_subst(
+        ty("a -> b"),
+        ty("a -> c"),
+        Substitution::singleton("a".to_string(), ty("Int")),
+    );
+}
